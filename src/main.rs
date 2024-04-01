041e8825205690f6c4c8aebf474e2e467c410b10
Max Herrera <mh1101110@gmail.com>
2024-04-01 09:29:51 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+struct Sudoku {
+    cells: [Cell; 81],
+}
+
+/* Structure:
+        bit 0: invalid bit
+        bit 1-9: can have numbers 1-9
+        bit 10-15: unused
+    */
+type Cell =i16
+
+
+fn generate_number(c: Cell) -> i32 {
+    for i in (1..9) {
+        println!(i);
+    }
+    return 0;
+}
+
+fn main() {
+    println!("\nHello, sudoku!\n");
+    let coolidea: Cell = Cell { state: 16 };
+    generate_number(coolidea);
+}
