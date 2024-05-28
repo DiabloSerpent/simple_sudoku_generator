e3f26cbcbf5a3ab7b28a11400ab76f1d20d28ef1
Max Herrera <mh1101110@gmail.com>
2024-05-27 22:15:10 -0500

    Remove comment

--- src/sudoku/intersection_removal.rs
+++ src/sudoku/intersection_removal.rs
@@ -12,47 +12,6 @@ impl Sudoku {
 
         let mut r = false;
 
-        /* Algorithm:
-
-            search by row, then col:
-                create array of 9 vectors, corresponding to each digit
-
-                continue if unsolved count in section < 2
-
-                search by cell in section:
-                    continue if cell is solved
-
-                    search by digit in cell:
-                        if count of digit in this section <= 3
-                            && count of digit in box_of(cell) > 2:
-                                array[digit].push(cell)
-
-                for vector in array:
-                    let d = current digit
-
-                    // if vector isnt empty, it should have >1 element
-                    if vector is empty:
-                        continue
-
-                    let box = box_of(vector[0])
-                    if vector[1..].iter().position(|&x| box_of(x) != box) == None {
-                        continue;
-                    }
-
-                    for cell in of_box(box):
-                        if cell has digit:
-                            r = true;
-                            cell.remove_digit(d)
-
-
-                //if the only 2 or 3 cells that hold a particular digit are in the same box,
-                //then clear out that digit from the rest of the cells in that box
-
-            search by box:
-                if the only 2 or 3 cells that hold a particular digit are in the same row/col,
-                then clear out that digit from the rest of the cells in that row/col
-        */
-
         for si in 0..18 {
             // Only check section if there are at least 2 unsolved cells
             if self.section_digit_sum[si][0] < 2 {
