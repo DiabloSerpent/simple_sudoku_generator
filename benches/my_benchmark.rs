b1589535e4525f83f7746315a8b37db2435436b9
Max Herrera <mh1101110@gmail.com>
2024-06-09 01:06:14 -0500

    Change benchmark config

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 use criterion::{criterion_group, criterion_main, Criterion};
 
 use simple_sudoku_generator::bench_main;
@@ -6,5 +7,12 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("Sudoku main", |b| b.iter(|| bench_main()));
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!{
+    name    = benches;
+    config  = Criterion::default()
+              .sample_size(500)
+              .warm_up_time(Duration::from_secs(10))
+              .measurement_time(Duration::from_secs(15));
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
